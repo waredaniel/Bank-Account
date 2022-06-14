c60650905255ed32b2d8aa8f34ce06f0c5e8476b
danielware <waredanielb@gmail.com>
2022-06-14 16:20:46 -0700

    Added banking functionality > > Co-authored-by: PJ Perry <pjperry28@gmail.com> > Co-authored-by: Derrak Richard <derrak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,9 @@ function Ledger(){
   this.currentId = 0
 }
 
-Ledger.prototype.addAccount = function(Account) {
+Ledger.prototype.addAccount = function(account) {
   account.id = this.assignId();
-  this.accountList[account.personName] = account;
+  this.accountList[account.id] = account;
 };
 
 Ledger.prototype.assignId = function() {
@@ -15,10 +15,34 @@ Ledger.prototype.assignId = function() {
   return this.currentId;
 };
 
+Ledger.prototype.findAccount = function (id) {
+  if (this.accountList[id] != undefined){
+    return this.accountList[id];
+  }
+  return false;
+}
+
+Ledger.prototype.deposite = function (id, depositeAmount) {
+newBalance = ledger.accountList[id].accountBalance + parseInt(depositeAmount);
+console.log(newBalance);
+ledger.accountList[id].accountBalance = newBalance;
+}
+
+Ledger.prototype.withdrawl = function (id, withdrawlAmount) {
+  newBalance = ledger.accountList[id].accountBalance - parseInt(withdrawlAmount);
+  ledger.accountList[id].accountBalance = newBalance;
+  console.log(newBalance);
+  }
+
 //Business Logic for Accounts
 
 function Account(personName, accountBalance) {
   this.personName = personName;
-  this.accountBalance = accountBalance;
+  this.accountBalance = parseInt(accountBalance);
 }
 
+let ledger = new Ledger();
+let account = new Account("Lovelace", "500");
+let account2 = new Account("Hopper", "199");
+ledger.addAccount(account);
+ledger.addAccount(account2);
